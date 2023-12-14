a813139c2ed7efd49268c32ae69093bf2b6222ee
Ole Kristian Heian Olaisen <ole.olaisen@vesselman.com>
2023-12-14 13:13:18 +0100

    Add dark theme support to DetailsScreen

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useTheme } from "react-native-paper";
 import { View, Text } from "react-native";
-
+import { ThemeContext } from "../context/themeContext";
 
 function DetailsScreen() {
- 
+  const { isDarkTheme, setIsDarkTheme } = useContext(ThemeContext);
+  const { colors } = useTheme();
   return (
     <View
       style={{
         flex: 1,
         alignItems: "center",
         justifyContent: "center",
-        
+        backgroundColor: colors.background,
       }}
     >
-      <Text>Details Screen</Text>
+      <Text style={{ color: colors.text }}>Details Screen</Text>
     </View>
   );
 }
