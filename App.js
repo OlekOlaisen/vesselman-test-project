77dd506e52fd23e43eea220bcb3d8457541be891
Ole Kristian Heian Olaisen <ole.olaisen@vesselman.com>
2023-12-15 11:45:58 +0100

    Add StatusBar component to App.js

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { StatusBar } from "react-native";
 import { PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import { lightTheme, darkTheme } from "./src/style/theme";
 import { ThemeContext } from "./src/context/themeContext";
 import NavigationTabs from "./src/components/NavigationTabs";
+
 
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
@@ -12,6 +14,14 @@ function App() {
   return (
     <PaperProvider theme={theme}>
       <ThemeContext.Provider value={{ isDarkTheme, setIsDarkTheme }}>
+        <StatusBar
+          barStyle={isDarkTheme ? "light-content" : "dark-content"}
+          backgroundColor={
+            isDarkTheme
+              ? darkTheme.colors.onBackground
+              : lightTheme.colors.onBackground
+          }
+        />
         <NavigationContainer>
           <NavigationTabs />
         </NavigationContainer>
