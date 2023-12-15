83c864f6e82ab1ef294ddabff05239a50794325f
Ole Kristian Heian Olaisen <ole.olaisen@vesselman.com>
2023-12-15 11:34:16 +0100

    Add react-native-svg and react-native-svg-transformer dependencies Add metro.config.js to configure SVG support Update ProfileScreen to display profile icon using SVG

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,21 +1,57 @@
 import React, { useContext } from "react";
 import { useTheme } from "react-native-paper";
-import { View, Text } from "react-native";
+import { View, Text, Image, StyleSheet } from "react-native";
 import { ThemeContext } from "../context/themeContext";
+import ProfileIcon from "../assets/profileIcon.svg";
+
 
 function ProfileScreen() {
   const { isDarkTheme, setIsDarkTheme } = useContext(ThemeContext);
   const { colors } = useTheme();
+
+  const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.background,
+    padding: 20,
+  },
+ profileImage: {
+  width: 150,
+ },
+  profileName: {
+    color: colors.text,
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  profileEmail: {
+    color: colors.text,
+    fontSize: 16,
+    marginBottom: 10,
+  },
+});
+
+
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor: colors.background,
-      }}
-    >
-      <Text style={{ color: colors.text }}>Profile</Text>
+    <View style={styles.container}>
+      <ProfileIcon
+        width={120}
+        height={120}
+        preserveAspectRatio="xMidYMid slice"
+        stroke={colors.primary}
+        fill={colors.primary}
+        strokeWidth={1}
+        
+      />
+
+      
+
+      <Text style={styles.profileName}>Name: John Doe</Text>
+      <Text style={styles.profileEmail}>Email: john.doe@example.com</Text>
     </View>
   );
 }
