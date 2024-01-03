c1070ae0599be2011fc05e59d3a7798edfdac605
Ole Kristian Heian Olaisen <ole.olaisen@vesselman.com>
2024-01-03 11:54:29 +0100

    Update HomeScreen layout and buttons

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
 import { useTheme } from "react-native-paper";
 import React, { useContext } from "react";
 import { ThemeContext } from "../context/themeContext";
@@ -15,13 +15,20 @@ function HomeScreen({navigation}) {
       backgroundColor: colors.background,
       padding: 20,
     },
+    row: {
+      flexDirection: "row",
+      justifyContent: "space-evenly",
+    },
     button: {
       backgroundColor: colors.primary,
       padding: 10,
       marginBottom: 5,
       width: 150,
+      height: 150,
       margin: 10,
       borderRadius: 10,
+      alignItems: "center",
+      justifyContent: "center",
     },
     buttonText: {
       ...fonts.button,
@@ -30,77 +37,84 @@ function HomeScreen({navigation}) {
 
 
  return (
-   <View
+   <ScrollView
      style={{
        flex: 1,
-       alignItems: "center",
-       justifyContent: "center",
        backgroundColor: colors.background,
      }}
    >
-     <TouchableOpacity
-       style={styles.button}
-       onPress={() => navigation.navigate("Profile")}
-     >
-       <Text style={styles.buttonText}>Specification</Text>
-     </TouchableOpacity>
+     <View style={styles.row}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={() => navigation.navigate("Profile")}
+       >
+         <Text style={styles.buttonText}>Specification</Text>
+       </TouchableOpacity>
 
-     <TouchableOpacity
-       style={styles.button}
-       onPress={() => navigation.navigate("Profile")}
-     >
-       <Text style={styles.buttonText}>Task Manager</Text>
-     </TouchableOpacity>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={() => navigation.navigate("Profile")}
+       >
+         <Text style={styles.buttonText}>Task Manager</Text>
+       </TouchableOpacity>
+     </View>
 
-     <TouchableOpacity
-       style={styles.button}
-       onPress={() => navigation.navigate("Profile")}
-     >
-       <Text style={styles.buttonText}>Inspection</Text>
-     </TouchableOpacity>
+     <View style={styles.row}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={() => navigation.navigate("Profile")}
+       >
+         <Text style={styles.buttonText}>Inspection</Text>
+       </TouchableOpacity>
 
-     <TouchableOpacity
-       style={styles.button}
-       onPress={() => navigation.navigate("Profile")}
-     >
-       <Text style={styles.buttonText}>Vessels</Text>
-     </TouchableOpacity>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={() => navigation.navigate("Profile")}
+       >
+         <Text style={styles.buttonText}>Vessels</Text>
+       </TouchableOpacity>
+     </View>
 
-     <TouchableOpacity
-       style={styles.button}
-       onPress={() => navigation.navigate("Profile")}
-     >
-       <Text style={styles.buttonText}>Procedures</Text>
-     </TouchableOpacity>
+     <View style={styles.row}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={() => navigation.navigate("Profile")}
+       >
+         <Text style={styles.buttonText}>Procedures</Text>
+       </TouchableOpacity>
 
-     <TouchableOpacity
-       style={styles.button}
-       onPress={() => navigation.navigate("Profile")}
-     >
-       <Text style={styles.buttonText}>Issues</Text>
-     </TouchableOpacity>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={() => navigation.navigate("Profile")}
+       >
+         <Text style={styles.buttonText}>Issues</Text>
+       </TouchableOpacity>
+     </View>
 
-     <TouchableOpacity
-       style={styles.button}
-       onPress={() => navigation.navigate("Profile")}
-     >
-       <Text style={styles.buttonText}>Project</Text>
-     </TouchableOpacity>
+     <View style={styles.row}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={() => navigation.navigate("Profile")}
+       >
+         <Text style={styles.buttonText}>Project</Text>
+       </TouchableOpacity>
 
-     <TouchableOpacity
-       style={styles.button}
-       onPress={() => navigation.navigate("Profile")}
-     >
-       <Text style={styles.buttonText}>Maintenance</Text>
-     </TouchableOpacity>
-
-     <TouchableOpacity
-       style={styles.button}
-       onPress={() => navigation.navigate("Profile")}
-     >
-       <Text style={styles.buttonText}>Risk</Text>
-     </TouchableOpacity>
-   </View>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={() => navigation.navigate("Profile")}
+       >
+         <Text style={styles.buttonText}>Maintenance</Text>
+       </TouchableOpacity>
+     </View>
+     <View style={styles.row}>
+       <TouchableOpacity
+         style={styles.button}
+         onPress={() => navigation.navigate("Profile")}
+       >
+         <Text style={styles.buttonText}>Risk</Text>
+       </TouchableOpacity>
+     </View>
+   </ScrollView>
  );
 }
 
